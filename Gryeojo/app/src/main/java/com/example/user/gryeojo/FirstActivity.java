6ce7f3cdd0111f739987553f60cc438e4f6203ee
kimsunkyung <skk095@naver.com>
2018-11-01 21:23:58 +0900

    resultActivity 삭제 / FirstActivity에 기능 병합

--- Gryeojo/app/src/main/java/com/example/user/gryeojo/FirstActivity.java
+++ Gryeojo/app/src/main/java/com/example/user/gryeojo/FirstActivity.java
@@ -6,7 +6,9 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
@@ -15,10 +17,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.user.gryeojo.Network.Task.ImageRequestTask;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.zomato.photofilters.geometry.Point;
 import com.zomato.photofilters.imageprocessors.Filter;
 import com.zomato.photofilters.imageprocessors.subfilters.BrightnessSubfilter;
@@ -27,23 +33,29 @@ import com.zomato.photofilters.imageprocessors.subfilters.SaturationSubfilter;
 import com.zomato.photofilters.imageprocessors.subfilters.ToneCurveSubfilter;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static com.example.user.gryeojo.MainActivity.getBase64String;
 
 public class FirstActivity extends AppCompatActivity {
     ImageView inputImageView;
-    ImageButton imageButton1;
-    ImageButton imageButton2;
-    ImageButton imageButton3;
-    ImageButton imageButton4;
+    ImageButton imageButton1, imageButton2, imageButton3, imageButton4;
     Bitmap decodedBitmap;
-    String base_str;
-    String input;
-    String filter;
-    String base_img;
+    String base_str, filter, base_img;
     SharedPreferences mPref;
-    Bitmap inputImage;
-    Bitmap outputImage1, outputImage2, outputImage3, outputImage4;
+    Bitmap outputImage1;
+    ImageView imageView;
+    String image = "";
+    String converImg = "";
+    String key = "";
+    String firstkey = "";
+    Button save;
+    Gson gson = new Gson();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +63,8 @@ public class FirstActivity extends AppCompatActivity {
         inputImageView = findViewById(R.id.input_imageView);
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        save = findViewById(R.id.save_photo);
+
 
         mPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         base_img = mPref.getString("image", "0");
@@ -58,11 +72,12 @@ public class FirstActivity extends AppCompatActivity {
 
         byte[] decodedByteArray = Base64.decode(base_img, Base64.NO_WRAP);
         decodedBitmap = BitmapFactory.decodeByteArray(decodedByteArray, 0, decodedByteArray.length);
-        decodedBitmap =decodedBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        decodedBitmap = decodedBitmap.copy(Bitmap.Config.ARGB_8888, true);
 
         Log.d("image", String.valueOf(decodedBitmap));
 
         inputImageView.setImageBitmap(decodedBitmap);
+
 
         imageButton1 = findViewById(R.id.origin_btn);
         imageButton2 = findViewById(R.id.first_btn);
@@ -73,25 +88,24 @@ public class FirstActivity extends AppCompatActivity {
         opts.inMutable = true;
 
 
-
         imageButton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                inputImageView.setImageBitmap(outputImage1);
+                inputImageView.setImageBitmap(decodedBitmap);
             }
         });
 
         imageButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               filter = "water";
+                filter = "water";
                 result(filter);
             }
         });
         imageButton3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               filter = "oil";
+                filter = "oil";
                 result(filter);
             }
         });
@@ -105,7 +119,7 @@ public class FirstActivity extends AppCompatActivity {
     }
 
 
-    public void result(String filter){
+    public void result(String filter) {
 
         base_str = getBase64String(decodedBitmap);
 
@@ -113,18 +127,84 @@ public class FirstActivity extends AppCompatActivity {
 
         editor.putString("img", base_str);
 
-        editor.putString("key",String.valueOf(System.currentTimeMillis()));
+        editor.putString("key", String.valueOf(System.currentTimeMillis()));
 
         editor.putString("filter", filter);
         editor.commit();
 
-        Intent intent = new Intent(FirstActivity.this, ResultActivity.class);
-        startActivity(intent);
+        server();
+    }
+
+    public void server() {
+
+        mPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        image = mPref.getString("img", "0");
+        firstkey = mPref.getString("key", "0");
+        filter = mPref.getString("filter", "0");
+        Log.d("key", firstkey);
+        Log.d("filter", filter);
+        Log.d("image", image);
+
+        ImageRequestTask requestTask = new ImageRequestTask(new ImageRequestTask.ImageRequestTaskHandler() {
+            @Override
+            public void onSuccessTask(String result) {
+                JsonObject jsonObject = new Gson().fromJson(result, JsonObject.class);
+
+                key = jsonObject.get("key").getAsString();
+
+                if (key.equals(firstkey)) {
+                    converImg = jsonObject.get("img").getAsString();
+                    byte[] decodedString = Base64.decode(converImg, Base64.DEFAULT);
+                    decodedBitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+
+                    inputImageView.setImageBitmap(decodedBitmap);
+                }
 
 
-        Intent intent2 = new Intent(FirstActivity.this, ResultActivity.class);
-        startActivity(intent2);
-   }
+            }
+
+            @Override
+            public void onFailTask() {
+                Toast.makeText(getApplicationContext(), "서버에서 불러오는데 실패하였습니다.", Toast.LENGTH_LONG);
+            }
+
+            @Override
+            public void onCancelTask() {
+                Toast.makeText(getApplicationContext(), "사용자가 해당 작업을 중지하였습니다.", Toast.LENGTH_LONG);
+            }
+
+        });
+
+
+        requestTask.execute("http://35.231.246.9/", "imageUpload", image, firstkey, filter);
+
+    }
+
+
+    public void save() {
+        String filename;
+        Date date = new Date(0);
+        SimpleDateFormat sdf = new SimpleDateFormat ("yyyyMMddHHmmss");
+        filename =  sdf.format(date);
+
+        try{
+            String path = Environment.getExternalStorageDirectory().toString();
+            OutputStream fOut = null;
+            File file = new File(path, "/DCIM/Screenshots/"+filename+".jpg");
+            fOut = new FileOutputStream(file);
+
+            decodedBitmap.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+
+            MediaStore.Images.Media.insertImage(getContentResolver()
+                    ,file.getAbsolutePath(),file.getName(),file.getName());
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
     // method for base64 to bitmap
@@ -141,6 +221,7 @@ public class FirstActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.menu, menu);
         return true;
     }
+
     //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -149,6 +230,7 @@ public class FirstActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.save_photo:
                 // User chose the "Settings" item, show the app settings UI...
+                save();
                 Toast.makeText(getApplicationContext(), "저장되었습니다.", Toast.LENGTH_SHORT).show();
                 return true;
 
@@ -160,6 +242,7 @@ public class FirstActivity extends AppCompatActivity {
 
         }
     }
-}
 
-/* https://stackoverflow.com/questions/18072448/how-to-save-image-in-shared-preference-in-android-shared-preference-issue-in-a */
+
+
+}
